99c466cb9b63fa6052a2392566598423ff0c341b
Kristen <kristengamelin@gmail.com>
2021-09-29 15:57:04 -0400

    GetLetters created in .Service

--- Letters.Data/Letter.cs
+++ Letters.Data/Letter.cs
@@ -12,12 +12,12 @@ namespace Letters.Data
         [Key]
         public int LetterId { get; set; }
         [Required]
-        public int MyProperty { get; set; }
-        [Required]
         public Guid OwnerId { get; set; }
         [Required]
         [Display(Name = "Your Letter")]
         public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
         [Required]
         public DateTimeOffset CreatedUtc { get; set; }
         public DateTimeOffset? ModifiedUtc { get; set; }
---
+++ Letters.Services/LetterService.cs
@@ -0,0 +1,51 @@
+﻿using Letters.Data;
+using Letters.Models;
+using Letters.MVC.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Letters.Services
+{
+    public class LetterService
+    {
+        private readonly Guid _userId;
+        public LetterService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateLetter(LetterCreate model)
+        {
+            var entity = new Letter()
+            {
+                OwnerId = _userId,
+                Title = model.Title,
+                Content = model.Content,
+                CreatedUtc = DateTimeOffset.Now
+            };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Letters.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<LetterListItem> GetLetters()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query = ctx.Letters.Where(l => l.OwnerId == _userId).Select(l => new LetterListItem
+                {
+                    LetterId = l.LetterId,
+                    Title = l.Title,
+                    CreatedUtc = l.CreatedUtc
+                }
+                );
+                return query.ToArray();
+            }
+        }
+    }
+}
