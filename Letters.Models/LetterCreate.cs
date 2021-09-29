f1b12d8f94d1981e488d357efcd0ac9242826096
Kristen <kristengamelin@gmail.com>
2021-09-29 13:57:17 -0400

    fixed error in Letters browser

--- Letters.Models/LetterCreate.cs
+++ Letters.Models/LetterCreate.cs
@@ -9,11 +9,13 @@ namespace Letters.Models
 {
     public class LetterCreate
     {
+
         [Required]
         [MinLength(2, ErrorMessage ="Please enter at least 2 characters.")]
         [MaxLength(100, ErrorMessage ="There are too many characters in this fiels.")]
         public string Title { get; set; }
         [MaxLength(8000)]
         public string Content { get; set; }
+        public int LetterId { get; set; }
     }
 }
